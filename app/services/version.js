3506c9a7e9b219ddff140aa4d4ee3b95b65b4a81
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-07-02 00:12:29 -0300

    Detectando nwjs para evitar problema de deploy.

--- app/services/version.js
+++ app/services/version.js
@@ -4,8 +4,10 @@ export default Ember.Service.extend({
   version: null,
 
   getVersion: function() {
-    var config = require('./package.json');
-    this.set('version', config.version);
+    if (var isNodeWebkit = (typeof process == "object")) {
+      var config = require('./package.json');
+      this.set('version', config.version);
+    }
   }.on('init'),
 
 });
