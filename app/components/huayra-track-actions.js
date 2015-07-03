c539082ee413375dbf0770140064bf3cbbb2f219
Mauro <lavaramano@gmail.com>
2015-07-03 16:28:02 -0300

    agrego limpiar y pintar

---
+++ app/components/huayra-track-actions.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['huayra-track-actions'],
+  actions: {
+    clean: function(){
+      var track = this.get('track');
+
+      track.steps.forEach((s) => {
+        Ember.set(s, "active", false);
+      });
+    },
+
+    pintar: function(){
+      var track = this.get('track');
+      Ember.set(track, "paint", !track.paint);
+    }
+  },
+});
