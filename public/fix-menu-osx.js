7b8f200269ca5c4d6d22de60c365edc07e532e7d
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-07-17 11:51:40 -0300

    arreglos para que los paths funcionen en huayra.

--- public/fix-menu-osx.js
+++ public/fix-menu-osx.js
@@ -1,13 +1,13 @@
 var is_nodewebkit = (typeof process == "object");
 
 if (is_nodewebkit) {
-  var gui = require('nw.gui');
+  var gui = window.requireNode('nw.gui');
   win = gui.Window.get();
 
   var nativeMenuBar = new gui.Menu({type: "menubar"});
 
   try {
-    nativeMenuBar.createMacBuiltin("My App");
+    nativeMenuBar.createMacBuiltin("huayra-ritmos");
     win.menu = nativeMenuBar;
   } catch (ex) {
   }
