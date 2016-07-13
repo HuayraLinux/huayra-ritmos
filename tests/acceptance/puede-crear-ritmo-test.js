4a4beb17e472f0eeb3a1693d4f641b5f8001f1d8
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-07-13 13:20:06 -0300

    Creando un test de aceptación básico.

---
+++ tests/acceptance/puede-crear-ritmo-test.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'huayra-ritmos/tests/helpers/start-app';
+
+var application;
+
+module('Acceptance | puede crear ritmo', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /puede-crear-ritmo', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '');
+    click("#crear-proyecto")
+  });
+
+  andThen(function() {
+    click("#play-button");
+
+    setTimeout(() => {
+      click("#stop-button");
+    }, 1000);
+  });
+
+});
